ad735235bc9925bd2410e50d3f69e66f5df3ef53
Kirill <taranyuk31@mail.ru>
2024-07-30 16:24:41 +0400

    first commit

---
+++ task.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', () => {
+    const reveals = document.querySelectorAll('.reveal');
+  
+    reveals.forEach(reveal => {
+      const revealTop = reveal.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+  
+      if (revealTop < windowHeight) {
+        reveal.classList.add('reveal_active');
+      } else {
+        reveal.classList.remove('reveal_active');
+      }
+    });
+  });
